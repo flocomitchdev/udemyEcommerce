b1cacb954a61cfbcd8bd27ce24b77a932ed91d2a
flocomitchdev <pitchupcreations@gmail.com>
2024-02-23 17:15:17 +0100

    link to user_form ok, started user_form html

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -18,5 +19,10 @@ public class UserController {
         List<User> listUsers = userService.listAllUsers();
         userModel.addAttribute("listUsers", listUsers);
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String addUserTest(){
+        return "user_form";
     }
 }
