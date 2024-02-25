471613503c5930b9c64639e4436d03f78d62f754
flocomitchdev <pitchupcreations@gmail.com>
2024-02-25 10:55:04 +0100

    add spring secu (bypassed for now), encoding pass + test OK

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.shopme.admin.user;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,14 +15,23 @@ public class UserService {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public List<User> listAllUsers(){
         return (List<User>) userRepository.findAll();
     }
 
-    public void save(User user) {userRepository.save(user);
+    public void save(User user) {
+        encodePassword(user);
+        userRepository.save(user);
     }
 
     public List<Role> listAllROles(){
         return (List<Role>)roleRepository.findAll();
+    }
+
+    private void encodePassword(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
